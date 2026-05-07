b5b1c3e8e15374a2001fdcf2dee2aa6967d6286c
aiglesi6 <a_iglesias_12@hotmail.com>
2026-05-06 22:52:00 -0500

    added details to index

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,19 @@ import Home from '@/views/Home.vue'
 import Search from '@/views/Search.vue'
 import Results from '@/views/Results.vue'
 import Bookmarked from '@/views/Bookmarked.vue'
+import BookDetail from '@/views/BookDetail.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/search', name: 'Search', component: Search },
   { path: '/results', name: 'Results', component: Results },
-  { path: '/bookmarked', name: 'Bookmarked', component: Bookmarked }
+  { path: '/bookmarked', name: 'Bookmarked', component: Bookmarked },
+  {
+    path: '/book/:key',
+    name: 'BookDetail',
+    component: BookDetail,
+    props: true
+  }
 ]
 
 const router = createRouter({
